62a891389d0abd07a77af7daa90564fec013b1c8
MosDevx <mosesk.wamae@gmail.com>
2022-10-19 01:55:28 +0300

    Create Main Program Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+
+    fontFamily: {
+      display: ['Montserrat'],
+      body: ['Lato'],
+    },
+
+    extend: {
+      colors: {
+        primary: {
+          black: '#272A31',
+          orange: '#EC5242',
+          grey: '#535353',
+          'light-grey': '#f5f5f5',
+        },
+
+      },
+    },
   },
   plugins: [],
 };
