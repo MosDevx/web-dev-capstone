c7c5c061bd1a771d07b199eecc6c04c90df258b0
MosDevx <mosesk.wamae@gmail.com>
2022-10-18 16:32:53 +0300

    fix tailwind stylelint

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,19 +1,12 @@
 module.exports = {
   extends: ['stylelint-config-standard'],
-  rules: {
-    'at-rule-no-unknown': [
-      true,
-      {
-        ignoreAtRules: [
-          'tailwind',
-          'apply',
-          'variants',
-          'responsive',
-          'screen',
-        ],
-      },
+	rules: {
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': [
+        true,
+        {
+            ignoreAtRules: ['tailwind']
+        }
     ],
-    'declaration-block-trailing-semicolon': null,
-    'no-descending-specificity': null,
-  },
+},
 };
